43d30afb9236547feb554ebcc85721c272d64d53
Daniel Filho <danielfilho@64.20.183.184.nyc.electricfiber.net>
2011-09-14 04:40:25 -0400

    it works. navigation smooth and silk.

--- static/script.js
+++ static/script.js
@@ -1,6 +1,9 @@
-(function(){
+/*! http://mths.be/smoothscroll v1.5.2 by @mathias */
+(function(a,c){var b=(function(){var d=c(a.documentElement),f=c(a.body),e;if(d.scrollTop()){return d}else{e=f.scrollTop();if(f.scrollTop(e+1).scrollTop()==e){return d}else{return f.scrollTop(e)}}}());c.fn.smoothScroll=function(d){d=~~d||400;return this.find('a[href*="#"]').click(function(f){var g=this.hash,e=c(g);if(location.pathname.replace(/^\//,'')===this.pathname.replace(/^\//,'')&&location.hostname===this.hostname){if(e.length){f.preventDefault();b.stop().animate({scrollTop:e.offset().top},d,function(){location.hash=g})}}}).end()}}(document,jQuery));
+
+$(function(){
 	var screenHeight = window.innerHeight + 10;
-	for(var i=0; i<=document.querySelectorAll('.areas section').length; i++){
-		document.querySelectorAll('.areas section')[i].style.minHeight = screenHeight+'px';
-	}
-})();
+	$('.areas section').css('minHeight', screenHeight+10);
+
+	$('html').smoothScroll(500);
+});
