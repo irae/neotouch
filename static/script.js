4d1e9cb18d411504dd805a380acf97ea1086c4e3
Daniel Filho <danielfilho@64.20.183.184.nyc.electricfiber.net>
2011-09-14 00:28:26 -0400

    everything ok, just need to check the texts.

--- static/script.js
+++ static/script.js
@@ -1,1 +1,6 @@
-$('div.areas section').css('min-height', window.innerHeight+10);
+(function(){
+	var screenHeight = window.innerHeight + 10;
+	for(var i=0; i<=document.querySelectorAll('.areas section').length; i++){
+		document.querySelectorAll('.areas section')[i].style.minHeight = screenHeight+'px';
+	}
+})();
